b40541d14f854814b58ac7dfdf6e48e86e314151
Peter.Park <peter.park@freedgrouptech.com>
2023-03-22 10:56:53 +0900

    change after hotfix1

--- git-flow-test/src/app/page.tsx
+++ git-flow-test/src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
             Docs <span>-&gt;</span>
           </h2>
           <p className={inter.className}>
-            Find in-depth information about Next.js features and API.-update from develop branch
+            Find in-depth information about Next.js features and API.-update from develop branch-after hotfix merge
           </p>
         </a>
 
